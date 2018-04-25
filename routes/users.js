9673cea58338dfeab17faaff2d5b7edfd80deb1b
vvictorious <vvictorious30@gmail.com>
2018-04-25 16:37:20 -0600

    index page forms are up

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/user', function(req, res, next) {
+  res.send('submit info page');
+});
 module.exports = router;
